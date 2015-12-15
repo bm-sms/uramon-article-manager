0f607bbf95f66323d5f09542a1cc68bfccaed5f6
Fumiaki MATSUSHIMA <mtsmfm@gmail.com>
2015-12-15 16:06:57 +0900

    Add seeds

---
+++ db/seeds/development/sites.seeds.rb
@@ -0,0 +1,6 @@
+Site.create!(
+  name: 'site1',
+  fqdn: ENV['HEROKU_APP_NAME'].present? ? "#{ENV['HEROKU_APP_NAME']}.herokuapp.com" : 'localhost'
+)
+
+Site.create!(name: 'site2', fqdn: 'example.com')
