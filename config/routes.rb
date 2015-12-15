f9b0efb1254513d51ccb29b5d3ca1b2d5e8d8411
Fumiaki MATSUSHIMA <mtsmfm@gmail.com>
2015-12-15 16:46:35 +0900

    Redirect / to /admin

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   ActiveAdmin.routes(self)
 
+  root to: redirect('/admin')
+
   namespace :api do
     resources :sites, only: [], param: :fqdn do
       resources :articles, only: :index
