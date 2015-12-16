0540d1fd3da9eccb024746765b1f06f941baa109
Ryunosuke SATO <tricknotes.rs@gmail.com>
2015-12-16 16:19:43 +0900

    Include category to article JSON

--- app/controllers/api/articles_controller.rb
+++ app/controllers/api/articles_controller.rb
@@ -2,6 +2,6 @@ class Api::ArticlesController < ApplicationController
   def index
     articles = Site.find_by!(fqdn: params[:site_fqdn]).articles.published
 
-    render json: articles
+    render json: articles.as_json(include: :category)
   end
 end
